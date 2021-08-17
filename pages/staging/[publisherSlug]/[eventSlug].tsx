9bc78e40fd943b7b5fe64f397ffde740680e3e13
Cesar William Alvarenga <cesarwbr@gmail.com>
2021-08-17 14:54:11 -0300

    feat: staging events

---
+++ pages/staging/[publisherSlug]/[eventSlug].tsx
@@ -0,0 +1,56 @@
+import { FC } from "react";
+import { GetServerSideProps } from "next";
+
+const SSR: FC<{ html: string }> = ({ html }) => {
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <style global jsx>{`
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: "Austin News Text Roman", Georgia, Times, serif;
+          line-height: 25px;
+        }
+
+        blockquote {
+          background-image: url(https://www.telegraph.co.uk/etc/designs/telegraph/core/clientlibs/tmgchannels/_base/img/borders/comment__news.png);
+          line-height: 25px;
+          background-position: -1088px 0;
+          background-repeat: repeat-y;
+          color: #333;
+          font-family: "Austin News Text Roman", Georgia, Times, serif;
+          margin: 0 0 1.5rem;
+          padding: 0 2.5rem 0 2.6rem;
+        }
+
+        .summary--container {
+          padding-bottom: 0 !important;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { publisherSlug, eventSlug } = context.params;
+  const arenaMID = context.query.arena_mid;
+
+  let endpoint = `https://sandbox-staging.arena.im/ssr/${publisherSlug}/${eventSlug}`;
+
+  if (arenaMID) {
+    endpoint += "?arena_mid=" + arenaMID;
+  }
+
+  const data = await fetch(endpoint, {
+    headers: {
+      "user-agent": context.req.headers["user-agent"],
+    },
+  });
+
+  const html = await data.text();
+
+  return { props: { html } };
+};
+
+export default SSR;
